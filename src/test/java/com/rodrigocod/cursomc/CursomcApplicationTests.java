d598be3cf53e863668e35ac7ac78a45fefc1e877
Rodrig Pereira <peresernb@gmail.com>
2020-04-08 20:23:16 -0300

    Projeto criado

---
+++ src/test/java/com/rodrigocod/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigocod.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
