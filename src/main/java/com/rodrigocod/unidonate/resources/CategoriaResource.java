324ac778baab57110d672cbb206e6c776a49f895
Pereser <peresernb@gmail.com>
2020-05-03 19:56:58 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/rodrigocod/unidonate/resources/CategoriaResource.java
+++ src/main/java/com/rodrigocod/unidonate/resources/CategoriaResource.java
@@ -2,12 +2,16 @@ package com.rodrigocod.unidonate.resources;
 
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigocod.unidonate.domain.Categoria;
 import com.rodrigocod.unidonate.services.CategoriaService;
@@ -28,5 +32,14 @@ public class CategoriaResource {
 		
 	
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+		.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		}
 
 }
