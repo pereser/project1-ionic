42c8cdcab8bcb429be22b7c1db2622e6a50d0573
Pereser <peresernb@gmail.com>
2020-05-29 18:17:47 -0300

    Listando as Categorias Filtradas

--- src/main/java/com/rodrigocod/unidonate/services/CategoriaService.java
+++ src/main/java/com/rodrigocod/unidonate/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rodrigocod.unidonate.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,8 @@ public class CategoriaService {
 			
 		}
 		
+	}
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
